cd9f1586d469966e512ba7d32cb5361a1a9e11fc
Vincent du Plessis <vinduples@gmail.com>
2023-04-25 17:52:24 +0200

    signs jump on click prevention with a by separation and content insersion

--- signshow.js
+++ signshow.js
@@ -1,6 +1,8 @@
 document.querySelectorAll(".sign-container").forEach(container => {
     container.addEventListener("click", (e) => {
-        container.lastChild.previousSibling.classList.toggle("display");
+           console.log(container.lastChild.previousSibling.childNodes);
+        container.lastChild.previousSibling.childNodes[1].classList.toggle("display");
+     
         
     })
 });

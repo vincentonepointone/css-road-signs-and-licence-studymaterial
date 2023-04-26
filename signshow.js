7fd97c41d0262ce7501e65c072ec89d3e9b7cc19
Vincent du Plessis <vinduples@gmail.com>
2023-04-26 20:49:32 +0200

    webp images/ toggle display on all signs

--- signshow.js
+++ signshow.js
@@ -1,8 +1,28 @@
 document.querySelectorAll(".sign-container").forEach(container => {
     container.addEventListener("click", (e) => {
-           console.log(container.lastChild.previousSibling.childNodes);
         container.lastChild.previousSibling.childNodes[1].classList.toggle("display");
-     
-        
     })
 });
+let showText = true;
+
+document.getElementById("showAllButton").addEventListener("click", (e) => {
+    document.querySelectorAll(".sign-container").forEach((container) => {
+        let signCaption = container.lastChild.previousSibling.childNodes[1];
+        if(showText){
+        if (!signCaption.classList.contains("display")) {
+            signCaption.classList.toggle("display");
+        }        
+        } else {
+            if(signCaption.classList.contains("display")){
+                signCaption.classList.toggle("display")
+            }
+        }
+
+    });
+    if(showText){
+        document.getElementById("showAllButton").innerText=  "Hide All Meanings";
+    } else {
+        document.getElementById("showAllButton").innerText = "Show All Meanings";
+    }
+    showText= !showText;
+})
